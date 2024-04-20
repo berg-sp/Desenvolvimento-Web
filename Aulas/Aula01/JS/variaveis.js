8f1ea185b6b18151327d74049247bb0ef6d1339a
batistaberg <batista.berg@gmail.com>
2024-04-20 12:21:25 -0300

    Envio Aula01

---
+++ Aulas/Aula01/JS/variaveis.js
@@ -0,0 +1,19 @@
+var nome = "Fulano" //string
+var nome = String("Ciclano") //string
+
+var numero1 = 5 // number
+var numero2 = 5.5 // number
+var numero3 = Number("10") //number
+var numero4 = Number(100) // number
+var numero5 = parseInt(5.3) // number
+var numero6 = parseFloat(10.5) // number
+
+var ehMaiorDeIdade = true // boolean
+var ehVermelho = false // boolean
+
+var naoDefinido = undefined
+var vazio = null
+
+console.log(naoDefinido)
+console.log(vazio)
+console.log(naoEhNumero)
