89905bf60cacb5cf94bac1861eb06c551ead0a7e
batistaberg <batista.berg@gmail.com>
2024-05-31 00:18:29 -0300

    Atualizar lista 06-Web

---
+++ Listas/Lista06/js/exercicio01.js
@@ -0,0 +1,26 @@
+var pix = {
+    chavePix: 'claudia@empresa.br',
+    valorDaTransferencia: 1200,
+    dataDeTransacao: '31-05-2024',
+    descricaoDoPagamento: 'Pagamento utilizando o e-mail',
+    pagador: [
+        {
+            nome: "Claudia",
+            cpfOuCnpj: 12345678901
+        }
+    ],
+    recebedor: [
+        {
+            nome: "Rodrigo",
+            cpfOuCnpj: 23456789012
+        }
+    ],
+    transações: [
+        {
+            data: "21-05-2024",
+            valor: 40,
+            descrica: "Recarga celular"
+        }
+    ]
+}
+console.log(pix)
