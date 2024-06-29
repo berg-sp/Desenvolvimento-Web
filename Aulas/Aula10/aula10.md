83bb3d9a2eeae3c126704b03ab7347b0a2e67687
batistaberg <batista.berg@gmail.com>
2024-06-29 08:20:08 -0300

    Enviar Aula10-Web

---
+++ Aulas/Aula10/aula10.md
@@ -0,0 +1,1 @@
+# Modulos em Javascript
---
+++ Aulas/Aula10/js/script.js
@@ -0,0 +1,17 @@
+//import somar from './matematica/operacoes.js'
+//import * as operacoes from './matematica/operacoes.js'
+import { subtracao as Sub } from './matematica/operacoes.js'
+
+let subtracao = () => { }
+
+Sub(1, 2)
+
+//somar(1, 3)
+
+/*let imagem = document.getElementById('img')
+
+imagem.addEventListener('mouseenter', () => {
+    setTimeout(() => {
+        console.log('Enviando email para o(a)cliente')
+    }, 5000)
+})*/
