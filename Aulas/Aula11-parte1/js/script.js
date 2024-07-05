e21c332421beaf31730e862aeea9d0ad23f9b7aa
batistaberg <batista.berg@gmail.com>
2024-07-05 13:13:59 -0300

    Enviar Aula11-Web

---
+++ Aulas/Aula11-parte1/js/script.js
@@ -0,0 +1,28 @@
+
+let inputValorBruto = document.getElementById('valor-bruto')
+let inputValorLiquido = document.getElementById('valor-liquido')
+let inputTaxa = documento.getElementById('valor-taxa')
+let botao = document.addEventListener('btn-calcular')
+
+botao.addEventListener('click', () => {
+    let valorBruto = Number(inputValorBruto.value)
+    let taxa = Number(inputTaxa.value)
+
+    if (valorBruto - taxa > 0) {
+        inputValorLiquido.Value
+
+        }
+        else {
+
+        }
+
+        else 
+    }
+
+    
+
+
+
+    inputValorLiquido.value = inputValorBruto - taxa
+
+})
---
+++ Aulas/Aula11-parte2/aula11.md
@@ -0,0 +1,4 @@
+# Animações com CSS
+
+### Referências
+[KeyFrames](https://www.w3schools.com/cssref/css3_pr_animation-keyframes.php)
